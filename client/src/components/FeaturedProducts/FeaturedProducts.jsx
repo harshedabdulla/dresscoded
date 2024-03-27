9f36b768d01e6644bb7b4b08eccc514afd329ace
harshedabdulla <harshed2004@gmail.com>
2024-03-27 23:41:11 +0530

    basic ui complete

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Card from '../Card/Card'
 import './FeaturedProducts.scss'
-// import useFetch from '../../hooks/useFetch'
+import useFetch from '../../hooks/useFetch'
 
 const FeaturedProducts = ({ type }) => {
-  //   const { data, loading, error } = useFetch(
-  //     `/products?populate=*&[filters][type][$eq]=${type}`
-  //   )
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  )
 
   return (
     <div className="featuredProducts">
@@ -21,11 +21,11 @@ const FeaturedProducts = ({ type }) => {
         </p>
       </div>
       <div className="bottom">
-        {/* {error
+        {error
           ? 'Something went wrong!'
           : loading
           ? 'loading'
-          : data?.map((item) => <Card item={item} key={item.id} />)} */}
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   )
